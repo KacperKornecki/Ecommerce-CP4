047fdf2ec413f6d1b86218d166811e070d2a8e5a
en-013-24 <en-013-24@gumiforest.uek.krakow.pl>
2023-03-06 12:50:16 +0100

    'Greeter'

--- src/main/java/pl/kacperkornecki/Main.java
+++ src/main/java/pl/kacperkornecki/Main.java
@@ -1,8 +1,26 @@
 package pl.kacperkornecki;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void  main(String[] args) {
-        System.out.println("Hello there");
+        final List<String> names = Arrays.asList("Jakub", "Michal", "Agnieszka", "Ola", "Kasia");
+
+        Greeter greeter = new Greeter();
+        greeter.greet("Jakub"); // -> Hello Jakub
+
+        //Greet just ladies here
+        List<String> ladies = new ArrayList<String>();
+        for (String name : names) {
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+        for (String ladyName: ladies) {
+            System.out.println(ladyName);
+        }
     }
 }
 
